2b67ca0b2d86dcfbabb9b50aa295340b24f56eb6
cheveniko <88566759+Cheveniko@users.noreply.github.com>
2024-07-10 23:11:27 -0500

    add code formatting

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 <main class="pt-[71px]">
 	<!-- <h1 class="text-9xl text-slate-600 hover:text-red-50">OLA</h1> -->
-	<?php //the_content(); ?>
+	<?php //the_content(); 
+	?>
 	<div class="container py-8 gap-y-4">
 		<h1 class="text-6xl mb-4">Contenido no encontrado</h1>
 		<a href="<?php echo get_home_url(); ?>" class="bg-ave-primary text-2xl rounded-md px-4 py-2 text-white w-fit mb-4 hover:opacity-80 transition-opacity">Volver al inicio</a>
--- single-tc_events.php
+++ single-tc_events.php
@@ -1,66 +1,54 @@
 <?php get_header(); ?>
-<?php 
-    $title = get_the_title();
-	$quito = false;
-	$latacunga = false;
-	$ambato = false;
-	$riobamba = false;
-	if ($title == "Maduro Irresponsable Quito") $quito = true;  
-	if ($title == "Maduro Irresponsable Ambato") $ambato = true;  
-	if ($title == "Maduro Irresponsable Riobamba") $riobamba = true;  
+<?php
+$title = get_the_title();
+$quito = false;
+$latacunga = false;
+$ambato = false;
+$riobamba = false;
+if ($title == "Maduro Irresponsable Quito") $quito = true;
+if ($title == "Maduro Irresponsable Ambato") $ambato = true;
+if ($title == "Maduro Irresponsable Riobamba") $riobamba = true;
 ?>
+
 <main class="pt-[71px]">
 	<section>
-		<img
-			src="<?php the_field( 'event_banner_mobile' ); ?>"
-			class="block w-full sm:hidden"
-			alt="Banner evento"
-		/>
-		<img
-			src="<?php the_field( 'event_banner_desktop' ); ?>"
-			class="hidden w-full sm:block"
-			alt="Banner evento"
-		/>
+		<img src="<?php the_field('event_banner_mobile'); ?>" class="block w-full sm:hidden" alt="Banner evento" />
+		<img src="<?php the_field('event_banner_desktop'); ?>" class="hidden w-full sm:block" alt="Banner evento" />
 	</section>
 	<div class="container flex flex-col sm:flex-row justify-start text-sm sm:text-base my-4 gap-2 text-[#5d5d5d]">
 		<div class="flex gap-x-2">
-			<img
-				src="<?php bloginfo( 'template_url' ); ?>/assets/svg/calendar.svg"
-				alt="Fecha evento"
-			/>
+			<img src="<?php bloginfo('template_url'); ?>/assets/svg/calendar.svg" alt="Fecha evento" />
 			<p class=" min-w-max">
-				<?php $event_date = do_shortcode( '[tc_event_date]' );
-					echo strtoupper(
-					$event_date ) 
+				<?php $event_date = do_shortcode('[tc_event_date]');
+				echo strtoupper(
+					$event_date
+				)
 				?>
 			</p>
 		</div>
 		<div class="flex gap-x-2">
-			<img
-				src="<?php bloginfo( 'template_url' ); ?>/assets/svg/location.svg"
-				alt="Ubicación evento"
-			/>
+			<img src="<?php bloginfo('template_url'); ?>/assets/svg/location.svg" alt="Ubicación evento" />
 			<p class="min-w-max">
-				<?php $event_location = do_shortcode( '[tc_event_location]' );
-					echo
-					strtoupper( $event_location )
-				 ?>
+				<?php $event_location = do_shortcode('[tc_event_location]');
+				echo
+				strtoupper($event_location)
+				?>
 			</p>
 		</div>
 	</div>
 	<section class="container">
-			<h1 class="text-5xl sm:text-6xl font-semibold"><?php the_title(); ?></h1>
-			<div class="my-4">
-				<?php the_content(); ?>
-			</div>
-			<?php
-			$event_tickets = get_field( "event_tickets" );
-			if ( $event_tickets ) {
-				echo $event_tickets;
-			}
-			?>
+		<h1 class="text-5xl sm:text-6xl font-semibold"><?php the_title(); ?></h1>
+		<div class="my-4">
+			<?php the_content(); ?>
+		</div>
+		<?php
+		$event_tickets = get_field("event_tickets");
+		if ($event_tickets) {
+			echo $event_tickets;
+		}
+		?>
 	</section>
-	<section class="container py-8"> 
+	<section class="container py-8">
 		<h2 class="text-5xl font-semibold">Otras Funciones</h2>
 		<div class="grid grid-cols-3 grid-flow-row justify-between items-center gap-6 mt-4">
 			<a href="https://tickets.foco-estudio.com/event/ave-uio" class="border text-center px-4 py-8 col-span-3 sm:col-span-2 lg:col-span-1 flex flex-col border-[#c2c2c2] rounded-lg <?php if ($quito) echo "border-ave-primary border-2" ?>">
